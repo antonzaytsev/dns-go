445e6140616c206f6b0c31d3aa8fc2c0b841f3ec
Anton <anton.zaytsev@jiffyshirts.com>
2025-08-23 19:35:09 +0300

    Add 404 page for non / requests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import NotFound from './components/NotFound';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
